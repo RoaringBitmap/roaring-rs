f3073667f1e32e7abc41980fe5b7470c9e26ad3c
travis@localhost <travis@localhost>
2014-12-28 12:18:56 +0000

    Add API docs for 0.0.7

---
+++ docs/0.0.7/implementors/core/iter/trait.Extend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['roaring'] = ["<a class='stability Unstable' title='Unstable: just renamed as part of collections reform'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.u32.html'>u32</a>&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
