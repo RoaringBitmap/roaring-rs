ba684305f4076313f8735c96d3c0cdcc67b845f2
Wim Looman <ghostunderscore@gmail.com>
2014-12-28 23:38:31 +1300

    Add v0.0.1 API docs

---
+++ docs/v0.0.1/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['roaring'] = {"items":[[0,"","roaring",""],[1,"RoaringBitmap","",""],[10,"new","","",0],[10,"set","","",0],[10,"get","","",0],[10,"none","","",0],[10,"any","","",0],[10,"cardinality","","",0]],"paths":[[1,"RoaringBitmap"]]};
+initSearch(searchIndex);
