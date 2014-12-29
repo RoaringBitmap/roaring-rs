2ffa96576d2eee946072dfc1e794f672452ca450
Wim Looman <ghostunderscore@gmail.com>
2014-12-29 13:45:56 +1300

    Must name remote to set url for

--- save_credentials.sh
+++ save_credentials.sh
@@ -8,4 +8,4 @@ REPO_SLUG=${2:?'Must provide repo slug'}
 
 echo "machine github.com login $GITHUB_TOKEN password x-oauth-basic" >> ~/.netrc
 chmod 0600 ~/.netrc
-git remote set-url --push "https://github.com/$REPO_SLUG"
+git remote set-url --push github "https://github.com/$REPO_SLUG"
