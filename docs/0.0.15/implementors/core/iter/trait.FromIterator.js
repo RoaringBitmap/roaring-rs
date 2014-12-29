9e9415a63af6dc0866790ff7ca0d5c70212a6c79
travis@localhost <travis@localhost>
2014-12-29 00:49:38 +0000

    Add API docs for 0.0.15

---
+++ docs/0.0.15/implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['roaring'] = ["<a class='stability Unstable' title='Unstable: may be replaced by a more general conversion trait'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.u32.html'>u32</a>&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
