4e967a46de84364f7656f9defc790de1a08e5c87
Wim Looman <ghostunderscore@gmail.com>
2014-12-29 13:43:59 +1300

    Must use https url to push

--- save_credentials.sh
+++ save_credentials.sh
@@ -4,6 +4,8 @@ set -e
 
 : ${TRAVIS:?'This should only be run on Travis CI'}
 GITHUB_TOKEN=${1:?'Must provide github token'}
+REPO_SLUG=${2:?'Must provide repo slug'}
 
 echo "machine github.com login $GITHUB_TOKEN password x-oauth-basic" >> ~/.netrc
 chmod 0600 ~/.netrc
+git remote set-url --push "https://github.com/$REPO_SLUG"
