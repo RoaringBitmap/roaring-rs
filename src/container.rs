58f048ce73ebdcceb31d423ca06af2d7a84a433a
Wim Looman <ghostunderscore@gmail.com>
2015-01-03 19:37:14 +1300

    Add tests for iter/from_iter/clone and fix bug in bitmap iter

--- src/container.rs
+++ src/container.rs
@@ -1,4 +1,4 @@
-use std::{ u16, u32 };
+use std::{ u16, u32, uint };
 use std::fmt::{ Show, Formatter, Result };
 
 use store::Store;
@@ -138,7 +138,7 @@ impl<'a> BitmapIter<'a> {
     fn new(bits: &'a [u32; 2048]) -> BitmapIter<'a> {
         BitmapIter {
             key: 0,
-            bit: 0,
+            bit: uint::MAX,
             bits: bits,
         }
     }
@@ -147,25 +147,17 @@ impl<'a> BitmapIter<'a> {
 impl<'a> Iterator<u16> for BitmapIter<'a> {
     fn next(&mut self) -> Option<u16> {
         loop {
-            if self.key == 2049 {
-                break;
+            if self.key == 2047 && self.bit == (u32::BITS - 1) {
+                return None;
             }
             self.bit += 1;
             if self.bit == u32::BITS {
                 self.bit = 0;
                 self.key += 1;
             }
-            if self.key == 2048 {
-                break;
-            }
             if (self.bits[self.key] & (1 << self.bit)) != 0 {
-                break;
+                return Some((self.key * u32::BITS + self.bit) as u16);
             }
-        }
-        if self.key == 2049 {
-            None
-        } else {
-            Some((self.key * u32::BITS + self.bit) as u16)
         }
     }
 }
---
+++ tests/iter.rs
@@ -0,0 +1,34 @@
+extern crate roaring;
+use roaring::RoaringBitmap;
+
+#[test]
+fn array() {
+    let original: RoaringBitmap = FromIterator::from_iter(0..2000);
+    let clone: RoaringBitmap = FromIterator::from_iter(original.iter());
+
+    assert_eq!(clone, original);
+}
+
+#[test]
+fn bitmap() {
+    let original: RoaringBitmap = FromIterator::from_iter(0..6000);
+    let clone: RoaringBitmap = FromIterator::from_iter(original.iter());
+
+    assert_eq!(clone, original);
+}
+
+#[test]
+fn arrays() {
+    let original: RoaringBitmap = FromIterator::from_iter((0..2000).chain(1000000..1002000).chain(2000000..2001000));
+    let clone: RoaringBitmap = FromIterator::from_iter(original.iter());
+
+    assert_eq!(clone, original);
+}
+
+#[test]
+fn bitmaps() {
+    let original: RoaringBitmap = FromIterator::from_iter((0..6000).chain(1000000..1012000).chain(2000000..2010000));
+    let clone: RoaringBitmap = FromIterator::from_iter(original.iter());
+
+    assert_eq!(clone, original);
+}
