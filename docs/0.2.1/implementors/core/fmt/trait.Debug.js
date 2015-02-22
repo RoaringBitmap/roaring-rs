64ce9e57cc7f3684ad39e0309d4f949f95b65bcc
travis@localhost <travis@localhost>
2015-02-22 05:59:06 +0000

    Add API docs for 0.2.1

---
+++ docs/0.2.1/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['roaring'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;Size: ExtInt + Halveable + <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/0.2.1/implementors/core/ops/trait.BitXor.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['roaring'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;Size: ExtInt + Halveable&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, Size: ExtInt + Halveable&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;&amp;'a <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, Size: ExtInt + Halveable&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;&gt; for &amp;'a <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, 'b, Size: ExtInt + Halveable&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;&amp;'a <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;&gt; for &amp;'b <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
