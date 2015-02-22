56411779ad493de646589421bb4b68196092b928
Wim Looman <ghostunderscore@gmail.com>
2015-02-22 18:30:04 +1300

    Single line some code

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,11 @@
 use std::{ u8, u16, u32 };
 use std::num::Int;
 
-pub enum Either<Left, Right> {
-    Left(Left),
-    Right(Right),
-}
-
+pub enum Either<Left, Right> { Left(Left), Right(Right) }
 
 pub trait Halveable {
     type HalfSize: ExtInt;
+
     fn split(self) -> (Self::HalfSize, Self::HalfSize);
     fn join(h1: Self::HalfSize, h2: Self::HalfSize) -> Self;
 }
@@ -21,30 +18,21 @@ pub trait ExtInt: Int + To64 + From + BitLength { }
 
 impl Halveable for u64 {
     type HalfSize = u32;
-    fn split(self) -> (u32, u32) {
-        ((self >> u32::BITS) as u32, self as u32)
-    }
-    fn join(h1: u32, h2: u32) -> u64 {
-        ((h1 as u64) << u32::BITS as u64) + (h2 as u64)
-    }
+
+    fn split(self) -> (u32, u32) { ((self >> u32::BITS) as u32, self as u32) }
+    fn join(h1: u32, h2: u32) -> u64 { ((h1 as u64) << u32::BITS as u64) + (h2 as u64) }
 }
 impl Halveable for u32 {
     type HalfSize = u16;
-    fn split(self) -> (u16, u16) {
-        ((self >> u16::BITS) as u16, self as u16)
-    }
-    fn join(h1: u16, h2: u16) -> u32 {
-        ((h1 as u32) << u16::BITS as u32) + (h2 as u32)
-    }
+
+    fn split(self) -> (u16, u16) { ((self >> u16::BITS) as u16, self as u16) }
+    fn join(h1: u16, h2: u16) -> u32 { ((h1 as u32) << u16::BITS as u32) + (h2 as u32) }
 }
 impl Halveable for u16 {
     type HalfSize = u8;
-    fn split(self) -> (u8, u8) {
-        ((self >> u8::BITS) as u8, self as u8)
-    }
-    fn join(h1: u8, h2: u8) -> u16 {
-        ((h1 as u16) << u8::BITS as u16) + (h2 as u16)
-    }
+
+    fn split(self) -> (u8, u8) { ((self >> u8::BITS) as u8, self as u8) }
+    fn join(h1: u8, h2: u8) -> u16 { ((h1 as u16) << u8::BITS as u16) + (h2 as u16) }
 }
 
 impl To64 for usize { #[inline] fn to64(self) -> u64 { self as u64 } }
