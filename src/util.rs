e44ee81724101a6a89ff590913f00504d2d5f470
Wim Looman <!@nemo157.com>
2015-04-29 23:14:30 +1200

    Add tests & fixes for Halveable

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,5 @@
 #![allow(missing_docs)]
 
-use std::mem::size_of;
 use std::num::ParseIntError;
 
 use num::traits::{ PrimInt, Num };
@@ -25,20 +24,20 @@ pub trait ExtInt:
 impl Halveable for u64 {
     type HalfSize = u32;
 
-    fn split(self) -> (u32, u32) { ((self >> size_of::<u32>()) as u32, self as u32) }
-    fn join(h1: u32, h2: u32) -> u64 { ((h1 as u64) << size_of::<u32>() as u64) + (h2 as u64) }
+    fn split(self) -> (u32, u32) { ((self / 0x1_00_00_00_00u64) as u32, self as u32) }
+    fn join(h1: u32, h2: u32) -> u64 { ((h1 as u64) * 0x1_00_00_00_00u64) + (h2 as u64) }
 }
 impl Halveable for u32 {
     type HalfSize = u16;
 
-    fn split(self) -> (u16, u16) { ((self >> size_of::<u16>()) as u16, self as u16) }
-    fn join(h1: u16, h2: u16) -> u32 { ((h1 as u32) << size_of::<u16>() as u32) + (h2 as u32) }
+    fn split(self) -> (u16, u16) { ((self / 0x1_00_00u32) as u16, self as u16) }
+    fn join(h1: u16, h2: u16) -> u32 { ((h1 as u32) * 0x1_00_00u32) + (h2 as u32) }
 }
 impl Halveable for u16 {
     type HalfSize = u8;
 
-    fn split(self) -> (u8, u8) { ((self >> size_of::<u8>()) as u8, self as u8) }
-    fn join(h1: u8, h2: u8) -> u16 { ((h1 as u16) << size_of::<u8>() as u16) + (h2 as u16) }
+    fn split(self) -> (u8, u8) { ((self / 0x1_00u16) as u8, self as u8) }
+    fn join(h1: u8, h2: u8) -> u16 { ((h1 as u16) * 0x1_00u16) + (h2 as u16) }
 }
 
 impl To64 for usize { #[inline] fn to64(self) -> u64 { self as u64 } }
@@ -66,4 +65,81 @@ impl ExtInt for u8 { }
 
 pub fn cast<T: To64, U: From>(n: T) -> U {
     From::from(n)
+}
+
+#[cfg(test)]
+mod test {
+    use super::{ Halveable };
+
+    #[test]
+    fn test_split_u16() {
+        assert_eq!((0x00u8, 0x00u8), Halveable::split(0x0000u16));
+        assert_eq!((0x00u8, 0x01u8), Halveable::split(0x0001u16));
+        assert_eq!((0x00u8, 0xFEu8), Halveable::split(0x00FEu16));
+        assert_eq!((0x00u8, 0xFFu8), Halveable::split(0x00FFu16));
+        assert_eq!((0x01u8, 0x00u8), Halveable::split(0x0100u16));
+        assert_eq!((0x01u8, 0x01u8), Halveable::split(0x0101u16));
+        assert_eq!((0xFFu8, 0xFEu8), Halveable::split(0xFFFEu16));
+        assert_eq!((0xFFu8, 0xFFu8), Halveable::split(0xFFFFu16));
+    }
+
+    #[test]
+    fn test_split_u32() {
+        assert_eq!((0x0000u16, 0x0000u16), Halveable::split(0x00000000u32));
+        assert_eq!((0x0000u16, 0x0001u16), Halveable::split(0x00000001u32));
+        assert_eq!((0x0000u16, 0xFFFEu16), Halveable::split(0x0000FFFEu32));
+        assert_eq!((0x0000u16, 0xFFFFu16), Halveable::split(0x0000FFFFu32));
+        assert_eq!((0x0001u16, 0x0000u16), Halveable::split(0x00010000u32));
+        assert_eq!((0x0001u16, 0x0001u16), Halveable::split(0x00010001u32));
+        assert_eq!((0xFFFFu16, 0xFFFEu16), Halveable::split(0xFFFFFFFEu32));
+        assert_eq!((0xFFFFu16, 0xFFFFu16), Halveable::split(0xFFFFFFFFu32));
+    }
+
+    #[test]
+    fn test_split_u64() {
+        assert_eq!((0x00000000u32, 0x00000000u32), Halveable::split(0x0000000000000000u64));
+        assert_eq!((0x00000000u32, 0x00000001u32), Halveable::split(0x0000000000000001u64));
+        assert_eq!((0x00000000u32, 0xFFFFFFFEu32), Halveable::split(0x00000000FFFFFFFEu64));
+        assert_eq!((0x00000000u32, 0xFFFFFFFFu32), Halveable::split(0x00000000FFFFFFFFu64));
+        assert_eq!((0x00000001u32, 0x00000000u32), Halveable::split(0x0000000100000000u64));
+        assert_eq!((0x00000001u32, 0x00000001u32), Halveable::split(0x0000000100000001u64));
+        assert_eq!((0xFFFFFFFFu32, 0xFFFFFFFEu32), Halveable::split(0xFFFFFFFFFFFFFFFEu64));
+        assert_eq!((0xFFFFFFFFu32, 0xFFFFFFFFu32), Halveable::split(0xFFFFFFFFFFFFFFFFu64));
+    }
+
+    #[test]
+    fn test_join_u16() {
+        assert_eq!(0x0000u16, Halveable::join(0x00u8, 0x00u8));
+        assert_eq!(0x0001u16, Halveable::join(0x00u8, 0x01u8));
+        assert_eq!(0x00FEu16, Halveable::join(0x00u8, 0xFEu8));
+        assert_eq!(0x00FFu16, Halveable::join(0x00u8, 0xFFu8));
+        assert_eq!(0x0100u16, Halveable::join(0x01u8, 0x00u8));
+        assert_eq!(0x0101u16, Halveable::join(0x01u8, 0x01u8));
+        assert_eq!(0xFFFEu16, Halveable::join(0xFFu8, 0xFEu8));
+        assert_eq!(0xFFFFu16, Halveable::join(0xFFu8, 0xFFu8));
+    }
+
+    #[test]
+    fn test_join_u32() {
+        assert_eq!(0x00000000u32, Halveable::join(0x0000u16, 0x0000u16));
+        assert_eq!(0x00000001u32, Halveable::join(0x0000u16, 0x0001u16));
+        assert_eq!(0x0000FFFEu32, Halveable::join(0x0000u16, 0xFFFEu16));
+        assert_eq!(0x0000FFFFu32, Halveable::join(0x0000u16, 0xFFFFu16));
+        assert_eq!(0x00010000u32, Halveable::join(0x0001u16, 0x0000u16));
+        assert_eq!(0x00010001u32, Halveable::join(0x0001u16, 0x0001u16));
+        assert_eq!(0xFFFFFFFEu32, Halveable::join(0xFFFFu16, 0xFFFEu16));
+        assert_eq!(0xFFFFFFFFu32, Halveable::join(0xFFFFu16, 0xFFFFu16));
+    }
+
+    #[test]
+    fn test_join_u64() {
+        assert_eq!(0x0000000000000000u64, Halveable::join(0x00000000u32, 0x00000000u32));
+        assert_eq!(0x0000000000000001u64, Halveable::join(0x00000000u32, 0x00000001u32));
+        assert_eq!(0x00000000FFFFFFFEu64, Halveable::join(0x00000000u32, 0xFFFFFFFEu32));
+        assert_eq!(0x00000000FFFFFFFFu64, Halveable::join(0x00000000u32, 0xFFFFFFFFu32));
+        assert_eq!(0x0000000100000000u64, Halveable::join(0x00000001u32, 0x00000000u32));
+        assert_eq!(0x0000000100000001u64, Halveable::join(0x00000001u32, 0x00000001u32));
+        assert_eq!(0xFFFFFFFFFFFFFFFEu64, Halveable::join(0xFFFFFFFFu32, 0xFFFFFFFEu32));
+        assert_eq!(0xFFFFFFFFFFFFFFFFu64, Halveable::join(0xFFFFFFFFu32, 0xFFFFFFFFu32));
+    }
 }
