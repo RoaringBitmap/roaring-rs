997afad3c85ac8cad3a1f68af35132373767ab2e
travis@localhost <travis@localhost>
2016-01-08 12:24:46 +0000

    Add API docs for 0.4.0

---
+++ docs/0.4.0/implementors/core/iter/trait.Extend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['roaring'] = ["impl&lt;Size: ExtInt + Halveable&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;Size&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;","impl&lt;'a, Size: ExtInt + Halveable + 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;&amp;'a Size&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/0.4.0/implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['roaring'] = ["impl&lt;Size: ExtInt + Halveable&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;Size&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;","impl&lt;'a, Size: ExtInt + Halveable + 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;&amp;'a Size&gt; for <a class='struct' href='roaring/struct.RoaringBitmap.html' title='roaring::RoaringBitmap'>RoaringBitmap</a>&lt;Size&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
