387773940dc76571b96095dfcabd50ea2f12ecf1
Wim Looman <wim@nemo157.com>
2016-12-19 20:30:55 +0100

    impl IntoIterator for &RoaringBitmap

--- tests/iter.rs
+++ tests/iter.rs
@@ -6,7 +6,7 @@ use std::iter::FromIterator;
 #[test]
 fn array() {
     let original = RoaringBitmap::from_iter(0..2000u32);
-    let clone = RoaringBitmap::from_iter(original.iter());
+    let clone = RoaringBitmap::from_iter(&original);
 
     assert_eq!(clone, original);
 }
@@ -14,7 +14,7 @@ fn array() {
 #[test]
 fn bitmap() {
     let original = RoaringBitmap::from_iter(0..6000u32);
-    let clone = RoaringBitmap::from_iter(original.iter());
+    let clone = RoaringBitmap::from_iter(&original);
 
     assert_eq!(clone, original);
 }
@@ -22,7 +22,7 @@ fn bitmap() {
 #[test]
 fn arrays() {
     let original = RoaringBitmap::from_iter((0..2000u32).chain(1000000..1002000).chain(2000000..2001000));
-    let clone = RoaringBitmap::from_iter(original.iter());
+    let clone = RoaringBitmap::from_iter(&original);
 
     assert_eq!(clone, original);
 }
@@ -30,7 +30,7 @@ fn arrays() {
 #[test]
 fn bitmaps() {
     let original = RoaringBitmap::from_iter((0..6000u32).chain(1000000..1012000).chain(2000000..2010000));
-    let clone = RoaringBitmap::from_iter(original.iter());
+    let clone = RoaringBitmap::from_iter(&original);
 
     assert_eq!(clone, original);
 }
