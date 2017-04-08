f10d72e035467530082af6eeeae060f9766f96b2
arthurprs <arthurprs@gmail.com>
2017-04-08 20:17:48 +0200

    Add remove_range()

--- tests/treemap_lib.rs
+++ tests/treemap_lib.rs
@@ -36,6 +36,18 @@ fn smoke() {
 }
 
 #[test]
+fn remove_range() {
+    let ranges = [0u64, 1, 63, 64, 65, 100, 4096 - 1, 4096, 4096 + 1, 65536 - 1];
+    for (i, &a) in ranges.iter().enumerate() {
+        for &b in &ranges[i..] {
+            let mut bitmap = RoaringTreemap::from_iter(0..65536 + 1);
+            assert_eq!(bitmap.remove_range(a..b), (b - a));
+            assert_eq!(bitmap, RoaringTreemap::from_iter((0..a).chain(b..65536 + 1)));
+        }
+    }
+}
+
+#[test]
 fn test_max() {
     let mut bitmap = RoaringTreemap::new();
     assert_eq!(bitmap.max(), None);
