5306fadcc811365d264a5902858813a4f0cc8807
travis@localhost <travis@localhost>
2017-04-08 21:08:29 +0000

    Add API docs for 0.5.2

---
+++ docs/0.5.2/implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["roaring"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a Store","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a Container","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='roaring/bitmap/struct.RoaringBitmap.html' title='roaring::bitmap::RoaringBitmap'>RoaringBitmap</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for <a class='struct' href='roaring/bitmap/struct.RoaringBitmap.html' title='roaring::bitmap::RoaringBitmap'>RoaringBitmap</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='roaring/treemap/struct.RoaringTreemap.html' title='roaring::treemap::RoaringTreemap'>RoaringTreemap</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for <a class='struct' href='roaring/treemap/struct.RoaringTreemap.html' title='roaring::treemap::RoaringTreemap'>RoaringTreemap</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
