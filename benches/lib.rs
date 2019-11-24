42ec354b8381683a5ac11385ec1f5a0b2250c2fa
Nassor Paulino da Silva <nassor@gmail.com>
2019-11-24 11:27:29 -0800

    better benchmarks using criterion crate

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,73 +1,74 @@
-#![feature(test)]
-
-extern crate test;
 extern crate roaring;
 
-use test::Bencher;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use roaring::RoaringBitmap;
 
-#[bench]
-fn create(b: &mut Bencher) {
-    b.iter(|| {
-        RoaringBitmap::new();
-    })
+fn create(c: &mut Criterion) {
+    c.bench_function("create", |b| {
+        b.iter(|| {
+            RoaringBitmap::new();
+        })
+    });
 }
 
-#[bench]
-fn insert1(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bitmap = RoaringBitmap::new();
-        bitmap.insert(1);
-        bitmap
-    })
-}
+fn insert(c: &mut Criterion) {
+    c.bench_function(
+        "insert 1", |b| {
+        b.iter(|| {
+            let mut bitmap = RoaringBitmap::new();
+            bitmap.insert(1);
+            bitmap
+        });
+    });
 
-#[bench]
-fn insert2(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bitmap = RoaringBitmap::new();
+    c.bench_function(
+        "insert 2", |b| {
+        b.iter(|| {
+            let mut bitmap = RoaringBitmap::new();
         bitmap.insert(1);
         bitmap.insert(2);
         bitmap
-    })
-}
-
-#[bench]
-fn is_subset(b: &mut Bencher) {
-    let bitmap: RoaringBitmap = (1..250).collect();
-    b.iter(|| test::black_box(bitmap.is_subset(&bitmap)))
-}
-
-#[bench]
-fn is_subset_2(b: &mut Bencher) {
-    let sub: RoaringBitmap = (1000..8196).collect();
-    let sup: RoaringBitmap = (0..16384).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
-}
-
-#[bench]
-fn is_subset_3(b: &mut Bencher) {
-    let sub: RoaringBitmap = (1000..4096).map(|x| x * 2).collect();
-    let sup: RoaringBitmap = (0..16384).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
-}
-
-#[bench]
-fn is_subset_4(b: &mut Bencher) {
-    let sub: RoaringBitmap = (0..17).map(|x| 1 << x).collect();
-    let sup: RoaringBitmap = (0..65536).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
-}
-
-#[bench]
-fn remove_range_bitmap(b: &mut Bencher) {
-    let mut sub: RoaringBitmap = (0..65536).collect();
-    b.iter(|| {
-        // carefully delete part of the bitmap
-        // only the first iteration will actually change something
-        // but the runtime remains identical afterwards
-        test::black_box(sub.remove_range(4096 + 1..65536));
-        assert_eq!(sub.len(), 4096 + 1);
+        });
     });
 }
+
+fn is_subset(c: &mut Criterion) {
+    c.bench_function("is_subset 1", |b| {
+        let bitmap: RoaringBitmap = (1..250).collect();
+        b.iter(|| black_box(bitmap.is_subset(&bitmap)))
+    });
+
+    c.bench_function("is_subset 2", |b| {
+        let sub: RoaringBitmap = (1000..8196).collect();
+        let sup: RoaringBitmap = (0..16384).collect();
+        b.iter(|| black_box(sub.is_subset(&sup)))
+    });
+
+    c.bench_function("is_subset 3", |b| {
+        let sub: RoaringBitmap = (1000..4096).map(|x| x * 2).collect();
+        let sup: RoaringBitmap = (0..16384).collect();
+        b.iter(|| black_box(sub.is_subset(&sup)))
+    });
+
+    c.bench_function("is_subset 4", |b| {
+        let sub: RoaringBitmap = (0..17).map(|x| 1 << x).collect();
+        let sup: RoaringBitmap = (0..65536).collect();
+        b.iter(|| black_box(sub.is_subset(&sup)))
+    });
+}
+
+fn remove_range_bitmap(c: &mut Criterion) {
+    c.bench_function("remove_range_bitmap", |b| {
+        let mut sub: RoaringBitmap = (0..65536).collect();
+        b.iter(|| {
+            // carefully delete part of the bitmap
+            // only the first iteration will actually change something
+            // but the runtime remains identical afterwards
+            black_box(sub.remove_range(4096 + 1..65536));
+            assert_eq!(sub.len(), 4096 + 1);
+        });
+    });
+}
+
+criterion_group!(benches, create, insert, is_subset, remove_range_bitmap);
+criterion_main!(benches);
