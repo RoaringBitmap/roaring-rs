10676c6847ee5197b584c296ba0f3de529b579a4
Artem Vorotnikov <artem@vorotnikov.me>
2021-01-05 17:49:10 +0300

    More tests

--- tests/treemap_serialization.rs
+++ tests/treemap_serialization.rs
@@ -1,7 +1,11 @@
 use roaring::RoaringTreemap;
 use std::iter::FromIterator;
 
-fn serialize_deserialize(dataset: Vec<u64>) {
+fn serialize_deserialize<Dataset, I>(dataset: Dataset)
+where
+    Dataset: IntoIterator<Item = u64, IntoIter = I>,
+    I: Iterator<Item = u64>,
+{
     let rb = RoaringTreemap::from_iter(dataset);
 
     let mut buffer = vec![];
@@ -15,11 +19,39 @@ fn serialize_deserialize(dataset: Vec<u64>) {
 }
 
 #[test]
-fn serialization_of_empty_treemap() {
+fn empty() {
     serialize_deserialize(vec![])
 }
 
 #[test]
-fn serialization_basic() {
+fn basic() {
     serialize_deserialize(vec![1, 2, 3, 4, 5, 100, 1000])
+}
+
+#[test]
+fn basic_2() {
+    serialize_deserialize(vec![1, 2, 3, 4, 5, 100, 1000, 10000, 100000, 1000000])
+}
+
+#[test]
+fn basic_3() {
+    let u32max = u32::MAX as u64;
+    serialize_deserialize(
+        vec![
+            1,
+            2,
+            3,
+            4,
+            5,
+            100,
+            1000,
+            10000,
+            100000,
+            1000000,
+            u32max + 10,
+            u32max << 10,
+        ]
+        .into_iter()
+        .chain(u32max..(u32max + 2 * (1 << 16))),
+    )
 }
