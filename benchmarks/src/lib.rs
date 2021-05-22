3678afe88177878d6ed8d02af019d508fce8a6ed
Kerollmops <clement@meilisearch.com>
2021-05-22 09:48:01 +0200

    Move the benchmarks into a sub-crate to isolate them from the library

---
+++ benchmarks/src/lib.rs
@@ -0,0 +1,5 @@
+//! This library is only used to isolate the benchmarks
+//! from the original roaring library.
+//!
+//! It does not include interesting functions for roaring library
+//! users only for roaring contributors.
