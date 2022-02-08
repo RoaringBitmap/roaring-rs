db76ae687e49ef6f4312436685ed3e92e0b77120
Kerollmops <clement@meilisearch.com>
2022-02-08 11:49:09 +0100

    Add a submodule of a set of real datasets for benchmarks

---
+++ benchmarks/benches/datasets_paths.rs
@@ -0,0 +1,14 @@
+#![allow(dead_code)]
+
+pub const CENSUS_INCOME: &str = "real-roaring-datasets/census-income";
+pub const CENSUS_INCOME_SRT: &str = "real-roaring-datasets/census-income_srt";
+pub const CENSUS1881: &str = "real-roaring-datasets/census1881";
+pub const CENSUS1881_SRT: &str = "real-roaring-datasets/census1881_srt";
+pub const DIMENSION_003: &str = "real-roaring-datasets/dimension_003";
+pub const DIMENSION_008: &str = "real-roaring-datasets/dimension_008";
+pub const DIMENSION_033: &str = "real-roaring-datasets/dimension_033";
+pub const USCENSUS2000: &str = "real-roaring-datasets/uscensus2000";
+pub const WEATHER_SEPT_85: &str = "real-roaring-datasets/weather_sept_85";
+pub const WEATHER_SEPT_85_SRT: &str = "real-roaring-datasets/weather_sept_85_srt";
+pub const WIKILEAKS_NOQUOTES: &str = "real-roaring-datasets/wikileaks-noquotes";
+pub const WIKILEAKS_NOQUOTES_SRT: &str = "real-roaring-datasets/wikileaks-noquotes_srt";
