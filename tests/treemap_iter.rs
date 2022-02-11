4d472dc1c203b998f1afc4096b2e02462bf52541
Jay Schmidek <jay@diffbot.com>
2022-02-11 15:20:05 -0800

    Implement DoubleEndedIterator for RoaringTreemap

--- tests/treemap_iter.rs
+++ tests/treemap_iter.rs
@@ -55,3 +55,60 @@ fn bitmaps_iterator() {
     assert_eq!(clone, original);
     assert_eq!(clone2, original);
 }
+
+#[test]
+fn rev() {
+    let original = (1..3)
+        .chain(1_000_000..1_012_003)
+        .chain(2_000_001..2_000_003)
+        .chain(2_000_000_000_001..2_000_000_000_003)
+        .rev()
+        .collect::<Vec<_>>();
+    let clone = RoaringTreemap::from_iter(original.clone()).iter().rev().collect::<Vec<_>>();
+
+    assert_eq!(clone, original);
+}
+
+#[test]
+fn double_ended() {
+    let mut original_iter = (1..3)
+        .chain(1_000_000..1_012_003)
+        .chain(2_000_001..2_000_003)
+        .chain(2_000_000_000_001..2_000_000_000_003);
+    let mut clone_iter = RoaringTreemap::from_iter(original_iter.clone()).into_iter();
+
+    let mut flip = true;
+    loop {
+        let (original, clone) = if flip {
+            (original_iter.next(), clone_iter.next())
+        } else {
+            (original_iter.next_back(), clone_iter.next_back())
+        };
+        assert_eq!(clone, original);
+        if original.is_none() {
+            break;
+        }
+        flip = !flip;
+    }
+
+    // Check again with one more element so we end with the other direction
+    let mut original_iter = (1..3)
+        .chain(1_000_000..1_012_003)
+        .chain(2_000_001..2_000_003)
+        .chain(2_000_000_000_001..2_000_000_000_004);
+    let mut clone_iter = RoaringTreemap::from_iter(original_iter.clone()).into_iter();
+
+    let mut flip = true;
+    loop {
+        let (original, clone) = if flip {
+            (original_iter.next(), clone_iter.next())
+        } else {
+            (original_iter.next_back(), clone_iter.next_back())
+        };
+        assert_eq!(clone, original);
+        if original.is_none() {
+            break;
+        }
+        flip = !flip;
+    }
+}
