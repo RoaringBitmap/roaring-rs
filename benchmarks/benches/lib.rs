16dbd0ea13264b6a8e7c7f36019c3dadf8c9f58b
saik0 <github@saik0.net>
2022-02-11 03:37:37 -0800

    rename deserialize_from_unvalidated to deserialize_from_unchecked

--- benchmarks/benches/lib.rs
+++ benchmarks/benches/lib.rs
@@ -382,18 +382,13 @@ fn deserialization(c: &mut Criterion) {
             });
         });
 
-        group.bench_function(
-            BenchmarkId::new("deserialize_from_nonvalidating", &dataset.name),
-            |b| {
-                b.iter(|| {
-                    for buf in input.iter() {
-                        black_box(
-                            RoaringBitmap::deserialize_from_nonvalidating(buf.as_slice()).unwrap(),
-                        );
-                    }
-                });
-            },
-        );
+        group.bench_function(BenchmarkId::new("deserialize_from_unchecked", &dataset.name), |b| {
+            b.iter(|| {
+                for buf in input.iter() {
+                    black_box(RoaringBitmap::deserialize_from_unchecked(buf.as_slice()).unwrap());
+                }
+            });
+        });
     }
 
     group.finish();
